365148a63b74ef8a5a46fd2dfefe34e98800bbb2
heexohee <78407631+heexohee@users.noreply.github.com>
2023-09-26 06:45:38 -0700

    Create 배열 회전시키기.jsx

---
+++ 배열 회전시키기.jsx
@@ -0,0 +1,19 @@
+// 배열 회전시키기
+
+// 문제 설명
+// 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다.
+// 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers, direction) {
+   if (direction === "right") {
+        const lastElement = numbers.pop(); 
+        numbers.unshift(lastElement); 
+    }
+    if (direction === "left") {
+         const firstElement = numbers.shift();
+         numbers.push(firstElement);   
+}
+      return numbers;
+}
+
+// js 기본 메서드 확인
